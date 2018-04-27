892c86c9535619c95621ae18a4e34099265a942f
SNU\yoonsukho <yoonsukho91@robotics.snu.ac.kr>
2018-04-27 16:07:00 +0900

    end effector marker updated / gripper joint changed to weld joint

--- srLib2017/src/robotManager/MH12RobotManager.cpp
+++ srLib2017/src/robotManager/MH12RobotManager.cpp
@@ -7,11 +7,11 @@ MH12RobotManager::MH12RobotManager(MH12Robot * robot, srSpace * space)
 	this->setEndeffector(&robot->gMarkerLink[MH12_Index::MLINK_GRIP]);
 
 	// gripper setting
-	vector<srJoint*> gripperJoint(3);
-	gripperJoint[0] = robot->gGripJoint[MH12_Index::GRIPJOINT_1];
-	gripperJoint[1] = robot->gGripJoint[MH12_Index::GRIPJOINT_2];
-	gripperJoint[2] = robot->gGripJoint[MH12_Index::GRIPJOINT_3];
-	this->setGripper(gripperJoint);
+	//vector<srJoint*> gripperJoint(3);
+	//gripperJoint[0] = robot->gGripJoint[MH12_Index::WELDJOINT_GRIPJOINT_2];
+	//gripperJoint[1] = robot->gGripJoint[MH12_Index::WELDJOINT_GRIPJOINT_2];
+	//gripperJoint[2] = robot->gGripJoint[MH12_Index::WELDJOINT_GRIPJOINT_2];
+	//this->setGripper(gripperJoint);
 
 	// sensor setting
 	//this->setFTSensor(robot->gWeldJoint[MH12_Index::WELDJOINT_COUPLING]);
